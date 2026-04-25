94ddce7ec86c7df99e93fdf798546d891a764cb7
Ali Yeganeh <ayeg0001@student.monash.edu>
2026-04-25 22:40:10 +1000

    .

--- app/src/main/java/com/example/esaver/LoginScreen.kt
+++ app/src/main/java/com/example/esaver/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.energysaver
 
 import android.R
+import android.widget.Button
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,9 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -29,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -95,6 +100,22 @@ fun LogInCompose(onLoginClick: () -> Unit,
                 ) {
                     Text(text = "Login",
                         fontSize = 22.sp)
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Button(onClick = {},
+                    modifier = Modifier.fillMaxWidth().height(55.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White,
+                        contentColor = Color.Black)){
+                    Row(verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center) {
+                        Icon(painter = painterResource(id = com.example.esaver.R.drawable.img),
+                            contentDescription = "Google sign in",
+                            tint = Color.Unspecified,
+                            modifier = Modifier.size(20.dp))
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text("Sign in with Google")
+
+                    }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
